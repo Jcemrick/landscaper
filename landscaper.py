6135b71616b549ac983c06c25e2266fd12c8d8a2
Joseph Emrick <jcemrick@gmail.com>
2023-02-07 13:35:02 -0800

    added game function and fixed some print errors

--- landscaper.py
+++ landscaper.py
@@ -1,41 +1,68 @@
 ## Setting the game state
 
-game = {"Cutting Implement" : 0, "Money": 0} 
+game = {"cutting implement" : 0, "money": 0} 
 
 ## Game items
 cutting_implements = [
-    {"name": "Teeth", "profit": 1, "cost": 0},
-    {"name": "Rusty Scissors", "profit": 5, "cost": 5},
-    {"name": "Push Powered Mower", "profit": 50, "cost": 25},
-    {"name": "Battery Powered Mower", "profit": 100, "cost": 250},
-    {"name": "Team of Starving Students", "profit": 250, "cost": 500},
+    {"name": "your teeth", "profit": 1, "cost": 0},
+    {"name": "rusty scissors", "profit": 5, "cost": 5},
+    {"name": "push powered mower", "profit": 50, "cost": 25},
+    {"name": "battery powered mower", "profit": 100, "cost": 250},
+    {"name": "team of starving students", "profit": 250, "cost": 500},
 ]
 
 ## Game functions
 
 def cut_grass():
-    cutting_implement = cutting_implements[game["Cutting Implement"]]
-    print(f"You cut the lawn with {cutting_implement['name']} and made {cutting_implement['profit']}!")
-    game["Money"] += cutting_implement["profit"]
+    cutting_implement = cutting_implements[game["cutting implement"]]
+    print(f"You cut the lawn with {cutting_implement['name']} and made ${cutting_implement['profit']}!")
+    game["money"] += cutting_implement["profit"]
 
 def check_stats():
-    cutting_implement = cutting_implements[game["Cutting Implement"]]
+    cutting_implement = cutting_implements[game["cutting implement"]]
     print(f"You currently have ${game['money']} and are using {cutting_implement['name']} to cut grass")
 
 def shop():
-    buy = cutting_implements[game["Cutting Implement"] + 1]
+    if (game["cutting implement"] >= len(cutting_implements) -1):
+        print("There is nothing left to buy!")
+        return 0
+
+    buy = cutting_implements[game["cutting implement"] + 1]
     if (buy == None):
         print('Nothing left to buy!')
         return 0
-    if (game["Money"] < buy["cost"]):
+    if (game["money"] < buy["cost"]):
         print('Not enough buy')
         return 0
-    game["Money"] -= buy["cost"]
-    game["Cutting Implement"] += 1
+    game["cutting implement"] += 1
+    game["money"] -= buy["cost"]
+    cutting_implement = cutting_implements[game["cutting implement"]]
+    print(f"You have successfully bought {cutting_implement['name']}")
 
 def win_check():
-    if(game["Cutting Implement"] == 4 and game["Money"] == 1000):
+    if(game["cutting implement"] == len(cutting_implements) -1 and game["money"] == 1000):
         print("You've Won!")
         return True
     return False
 
+while (True):
+
+    i = input("[1] Cut Some Grass [2] Check Stats [3] Shop [4] Quit Game ")
+
+    i = int(i)
+
+    if (i == 1):
+        cut_grass()
+    
+    if (i == 2):
+        check_stats()
+
+    if (i == 3):
+        shop()
+    
+    if (i == 4):
+        print("You have quit the game!")
+        break
+
+    if (win_check()):
+        break
