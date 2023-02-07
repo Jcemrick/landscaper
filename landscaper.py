897f1518db042c09b53ab2e7b005ba28ef71a34a
Joseph Emrick <jcemrick@gmail.com>
2023-02-07 12:50:32 -0800

    made game state and added game items

---
+++ landscaper.py
@@ -0,0 +1,15 @@
+## Setting the game state
+
+game = {"Cutting Implement" : 0, "Money": 0} 
+
+## Game items
+cutting_implements = [
+    {"name": "Teeth", "profit": 1, "cost": 0},
+    {"name": "Scissors", "profit": 3, "cost": 5},
+    {"name": "Large Shears", "profit": 6, "cost": 10},
+    {"name": "Weed Trimmer", "profit": 12, "cost": 50},
+    {"name": "Push Powered Mower", "profit": 24, "cost": 100},
+    {"name": "Gas Mower", "profit": 48, "cost": 200},
+    {"name": "Riding Mower", "profit": 106, "cost": 700},
+]
+
