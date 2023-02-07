a45690aac62d9ef9c20c1f6f76a44543692b9ecf
Joseph Emrick <jcemrick@gmail.com>
2023-02-07 12:59:08 -0800

    changed the game items to what is requested in the assignment

--- landscaper.py
+++ landscaper.py
@@ -5,11 +5,15 @@ game = {"Cutting Implement" : 0, "Money": 0}
 ## Game items
 cutting_implements = [
     {"name": "Teeth", "profit": 1, "cost": 0},
-    {"name": "Scissors", "profit": 3, "cost": 5},
-    {"name": "Large Shears", "profit": 6, "cost": 10},
-    {"name": "Weed Trimmer", "profit": 12, "cost": 50},
-    {"name": "Push Powered Mower", "profit": 24, "cost": 100},
-    {"name": "Gas Mower", "profit": 48, "cost": 200},
-    {"name": "Riding Mower", "profit": 106, "cost": 700},
+    {"name": "Rusty Scissors", "profit": 5, "cost": 5},
+    {"name": "Push Powered Mower", "profit": 50, "cost": 25},
+    {"name": "Battery Powered Mower", "profit": 100, "cost": 250},
+    {"name": "Team of Starving Students", "profit": 250, "cost": 500},
 ]
 
+## Game functions
+
+def cut_grass():
+    cutting_implement = cutting_implements[game["Cutting Implement"]]
+    print(f"You cut the lawn with {cutting_implement['name']} and made {cutting_implement['profit']}!")
+    game["Money"] += cutting_implement["profit"]
